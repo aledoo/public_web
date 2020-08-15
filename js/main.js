3f30c321fbe3d3527cac6784323c8ca45f4efb26
Alex Dolara <alexdolara@MacBook-Air-de-Alex-2418.local>
2020-08-15 17:39:00 +0200

    Back to normal

--- js/main.js
+++ js/main.js
@@ -1,177 +1,177 @@
-// const artWork = [
-//     {
-//         artworkTitle: "falling in love",
-//         typeformID: "bi6F9pEy",
-//         buttonText: "BUY 100€",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "elephant",
-//         typeformID: "BwS7hGo4",
-//         buttonText: "BUY 100€",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "bailaor",
-//         typeformID: "MWMpcKBS",
-//         buttonText: "BUY 100€",
-//         colors: "26",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "something going on",
-//         typeformID: "QHkceWuA",
-//         buttonText: "BUY 100€",
-//         colors: "23",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "still life",
-//         typeformID: "kSncHn",
-//         buttonText: "BUY 100€",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "jacqueline y las flores",
-//         typeformID: "tljae1",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "woman on a sofa",
-//         typeformID: "ynCgtQ",
-//         buttonText: "BUY 100€",
-//         colors: "32",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "portrait",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "rider",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "bottle",
-//         typeformID: "T97Las",
-//         buttonText: "BUY 100€",
-//         colors: "26",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "cheeses",
-//         typeformID: "fMgs1H",
-//         buttonText: "BUY 100€",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "applauses",
-//         typeformID: "FYxAvg",
-//         buttonText: "BUY 100€",
-//         colors: "17",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "travelling",
-//         typeformID: "x6819A",
-//         buttonText: "BUY 100€",
-//         colors: "24",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "shirts",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "jordan",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "the pianist",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     },
-//     {
-//         artworkTitle: "tango",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "",
-//         artSize: "",
-//         frameSize: "",
-//         paperQuality: ""
-//     },
-//     {
-//         artworkTitle: "cats",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "",
-//         artSize: "",
-//         frameSize: "3",
-//         paperQuality: ""
-//     },
-//     {
-//         artworkTitle: "women",
-//         typeformID: "LTflaQ",
-//         buttonText: "SOLD",
-//         colors: "25",
-//         artSize: "21x29,7",
-//         frameSize: "30x40",
-//         paperQuality: "90"
-//     }
+const artWork = [
+    {
+        artworkTitle: "falling in love",
+        typeformID: "bi6F9pEy",
+        buttonText: "BUY 100€",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "elephant",
+        typeformID: "BwS7hGo4",
+        buttonText: "BUY 100€",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "bailaor",
+        typeformID: "MWMpcKBS",
+        buttonText: "BUY 100€",
+        colors: "26",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "something going on",
+        typeformID: "QHkceWuA",
+        buttonText: "BUY 100€",
+        colors: "23",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "still life",
+        typeformID: "kSncHn",
+        buttonText: "BUY 100€",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "jacqueline y las flores",
+        typeformID: "tljae1",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "woman on a sofa",
+        typeformID: "ynCgtQ",
+        buttonText: "BUY 100€",
+        colors: "32",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "portrait",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "rider",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "bottle",
+        typeformID: "T97Las",
+        buttonText: "BUY 100€",
+        colors: "26",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "cheeses",
+        typeformID: "fMgs1H",
+        buttonText: "BUY 100€",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "applauses",
+        typeformID: "FYxAvg",
+        buttonText: "BUY 100€",
+        colors: "17",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "travelling",
+        typeformID: "x6819A",
+        buttonText: "BUY 100€",
+        colors: "24",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "shirts",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "jordan",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "the pianist",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    },
+    {
+        artworkTitle: "tango",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "",
+        artSize: "",
+        frameSize: "",
+        paperQuality: ""
+    },
+    {
+        artworkTitle: "cats",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "",
+        artSize: "",
+        frameSize: "3",
+        paperQuality: ""
+    },
+    {
+        artworkTitle: "women",
+        typeformID: "LTflaQ",
+        buttonText: "SOLD",
+        colors: "25",
+        artSize: "21x29,7",
+        frameSize: "30x40",
+        paperQuality: "90"
+    }
     
-//   ];
+  ];
 
 
 
@@ -227,22 +227,22 @@ function artTemplate(art) {
 };
 
 // Build the artwork list
-// document.getElementById("list").innerHTML = `
-//     ${artWork.map(artTemplate).join('')}
-// `
+document.getElementById("list").innerHTML = `
+    ${artWork.map(artTemplate).join('')}
+`
 
 // Fetch JSON
-fetch('data.json') 
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        // document.getElementById("list").innerHTML = `
-        //     ${data.map(artTemplate).join('')}
-        // ` 
-        console.log(data);
-        document.getElementById("list").innerHTML = `${data.map(artTemplate).join('')}`;
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+// fetch("data.json") 
+//     .then(function(resp) {
+//         return resp.json();
+//     })
+//     .then(function(data) {
+//         // document.getElementById("list").innerHTML = `
+//         //     ${data.map(artTemplate).join('')}
+//         // ` 
+//         console.log(data);
+//         document.getElementById("list").innerHTML = `${data.map(artTemplate).join('')}`;
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     });
