fdf00c96435502731dd3c13a0d350fe229042bbf
Alex Dolara <alexdolara@MacBook-Air-de-Alex-2418.local>
2020-08-17 15:49:58 +0200

    remove include

--- js/art_detail.js
+++ js/art_detail.js
@@ -44,8 +44,8 @@ function soldTextButtonChange(textChange) {
 // Build the list of artworks
 function artDetail(art) {
     var docTitle = document.title;
-    var artWorkTitle = art.artWorkTitle;
-    if (docTitle == includes(artWorkTitle)) {
+    var artWorkTitle = art.artWorkTitle
+    if (docTitle == artWorkTitle) {
         return `
         <div class="art__item__detail">
             <img src="img/artworks/${image(art.artworkTitle)}.jpg" height="350" alt="${(art.artworkTitle)}" />
