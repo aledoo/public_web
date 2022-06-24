ca2c8e3b8be39bafc4e80a524892eb57659d6de6
Alex Dolara <alexander.dolara@gmail.com>
2022-06-24 18:44:58 +0200

    new background

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-    var classCycle=['image-1','image-2'];
+    var classCycle=['image-1','image-2','image-3'];
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
     $('body').addClass(classToAdd);
