3e93aec8c42035fd154b67d09502e7ea2064b092
Alex Dolara <alexander.dolara@gmail.com>
2022-06-25 11:44:57 +0200

    Add event listener to background random image

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+document.addEventListener("DOMContentLoaded",function() {
     var classCycle=['image-1','image-2','image-3'];
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
